13659d8bbdbd6a9a4ee26388e94ebcd5ac020855
Abhay1kumar <kumarabhay401@gmail.com>
2022-01-23 21:06:17 +0530

    class 19 practice

--- script.js
+++ script.js
@@ -1,142 +1,28 @@
-//Array of object of employees
-let employees = [
-    {
-      id: 1,
-      first_name: "Suresh",
-      last_name: "Kumar",
-      email: "suresh@gmail.com",
-      gender: "Male",
-      ip_address: "160.192.178.21",
-    },
-  
-    {
-      id: 2,
-      first_name: "Sourav",
-      last_name: "Das",
-      email: "sourav@gmail.com",
-      gender: "Male",
-      ip_address: "160.182.178.21",
-    },
-  
-    {
-      id: 3,
-      first_name: "John",
-      last_name: "paul",
-      email: "john@gmail.com",
-      gender: "Male",
-      ip_address: "160.192.238.21",
-    },
-    {
-      id: 4,
-      first_name: "Pavan",
-      last_name: "Sai",
-      email: "pavan@gmail.com",
-      gender: "Male",
-      ip_address: "160.192.278.21",
-    },
-    {
-      id: 5,
-      first_name: "Sangeetha",
-      last_name: "Das",
-      email: "sangeetha@gmail.com",
-      gender: "Female",
-      ip_address: "160.162.178.21",
-    },
-    {
-      id: 6,
-      first_name: "Tushar",
-      last_name: "Vinocha",
-      email: "tushar@gmail.com",
-      gender: "Male",
-      ip_address: "160.092.178.21",
-    },
-    {
-      id: 7,
-      first_name: "Jenny",
-      last_name: "Paul",
-      email: "jenny@gmail.com",
-      gender: "Female",
-      ip_address: "160.192.178.21",
-    },
-  
-    {
-      id: 8,
-      first_name: "Jhansi",
-      last_name: "P",
-      email: "jhansi@gmail.com",
-      gender: "Female",
-      ip_address: "160.192.78.11",
-    },
-    {
-      id: 9,
-      first_name: "Lavish",
-      last_name: "Jain",
-      email: "lavish@gmail.com",
-      gender: "Male",
-      ip_address: "170.192.178.21",
-    },
-  
-    {
-      id: 10,
-      first_name: "Namrata",
-      last_name: "P",
-      email: "Namrata@gmail.com",
-      gender: "Female",
-      ip_address: "160.192.175.21",
-    },
-  ];
+let showthepassword=document.getElementById('check')
 
-//   all employees data
-  let allEmpbutton=document.querySelector('#all-emp');
-  allEmpbutton.addEventListener('click',function(){
-      displayEmployees(employees);
-  })
-
-//   male employees data 
-let MaleempButton=document.querySelector('#male-emp');
-MaleempButton.addEventListener('click',function(){
-    let maleemployee=employees.filter(function(employe){
-        return employe.gender == 'Male'
-    })
-    displayEmployees(maleemployee);
-})
-// female employees data
-let femaleempButton=document.querySelector('#female-emp');
-femaleempButton.addEventListener('click',function(){
-    let femaleemployee=employees.filter(function(employe){
-        return employe.gender == 'Female'
-    })
-    displayEmployees(femaleemployee);
-})
-
-let searchbox = document.querySelector('#search');
-
-searchbox.addEventListener("keyup",function(e){
-    let textEnter = searchbox.value;
-    let fileteremp=[];
-    if(textEnter!=""){
-fileteremp=employees.filter(function(employe){
-    return employe.first_name.toLowerCase().includes(textEnter.toLowerCase());
-})
-    }
-    displayEmployees(fileteremp);
-})
+showthepassword.addEventListener('click',function(){
+let typePassword=document.getElementById('password');
+let changetype=typePassword.getAttribute('type');
+if(changetype=='password'){
+  typePassword.setAttribute('type','text');
+} else{
+  typePassword.setAttribute('type','password');
+}
+});
 
 
-  let displayEmployees =(employees)=>{
-      let tablebody=document.querySelector('.table-body');
-    //   creat a which is empty and append to the table body
-    let tablerow="";
-    for (let employee of employees){
-        tablerow+=`<tr>
-        <td>${employee.id}</td>
-        <td>${employee.first_name}</td>
-        <td>${employee.last_name}</td>
-        <td>${employee.email}</td>
-        <td>${employee.gender}</td>
-        <td>${employee.ip_address}</td>
-        </tr>`;
-        tablebody.innerHTML=tablerow;
 
-    }
-  } 
+let likecount=0;
+let dislike= 0;
+
+let likebutton=document.getElementById('btn-like');
+likebutton.addEventListener('click',function(){
+likecount++;
+document.querySelector("#like").innerHTML=likecount;
+});
+
+let dislikebutton=document.getElementById('btn-dislike');
+dislikebutton.addEventListener('click',function(){
+  dislike++;
+  document.querySelector("#dislike").innerHTML=dislike;
+});
