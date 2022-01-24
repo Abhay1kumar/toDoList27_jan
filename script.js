4a66e072669f25372ff70f1cb00c6791fd8824a6
Abhay1kumar <kumarabhay401@gmail.com>
2022-01-24 17:46:43 +0530

    CRUD

--- script.js
+++ script.js
@@ -1,77 +1,82 @@
-let selectedRow = null;
+let url="https://61ea3a487bc0550017bc65f8.mockapi.io/user";
 
-function onFormSubmit() {
-  var formData = readFormData();
+function getdata(){
+  fetch(url,{
+    method:"GET",
+    header:{
+      "Content Type":"application/json"
+    },
+  })
+  .then((result)=>result.json())
+  .then((data)=>{
+    console.log(data);
+  })
+  .catch((error)=>{
+    console.log(error);
+  })
+}
+// getdata();
 
-  if (selectedRow == null) insertNewRecord(formData);
-  else updateRecord(formData);
-
-  resetForm();
+function creatData(){
+  let data={
+    name:"Abhay Kumar",
+    phone: "9999-895-695",
+    city:"Noida"
+  };
+  fetch(url, {
+    method:"POST",
+    body:JSON.stringify(data),
+    header:{
+      "Content Type":"application/json",
+    },
+  }).then((result)=>result.json())
+  .then((data)=>{
+    console.log(data);
+  })
+  .catch((error)=>{
+  console.log(error);
+  });
 }
 
-function readFormData() {
-  //create an empty object
-  const formData = {};
-  formData["fullname"] = document.getElementById("fullname").value;
-  formData["empCode"] = document.getElementById("empCode").value;
-  formData["salary"] = document.getElementById("salary").value;
-  formData["city"] = document.getElementById("city").value;
-  return formData;
-  // console.log(formData);
+// creatData();
+getdata();
+
+// upate the data
+function updateData(){
+  let data={
+    name:"Rohit kumar",
+    phone:"8593034884",
+    city:"Delhi"
+  };
+  fetch(url + "/4",{
+    method:"PUT",
+    body:JSON.stringify(data),
+    header:{
+      "Content Type": "application/json",
+    },
+  })
+  .then((result)=>result.json()).then((data)=>{
+    console.log(data)
+  })
+  .catch((error)=>{
+    console.log(error);
+  });
 }
 
-function insertNewRecord(data) {
-  let table = document
-    .getElementById("employeeList")
-    .getElementsByTagName("tbody")[0];
+// updateData();
 
-  //insert row
-  var newRow = table.insertRow(table.length);
-  cell1 = newRow.insertCell(0);
-  cell1.innerHTML = data.fullname;
-  cell2 = newRow.insertCell(1);
-  cell2.innerHTML = data.empCode;
-  cell3 = newRow.insertCell(2);
-  cell3.innerHTML = data.salary;
-  cell4 = newRow.insertCell(3);
-  cell4.innerHTML = data.city;
-  cell5 = newRow.insertCell(4);
-  cell5.innerHTML = `
-  <button class="btn btn-primary" onClick="onEdit(this)">Edit</button>
-  <button class="btn btn-primary" onClick="onDelete(this)">Delete</button>
-  `;
+// delete the data
+
+function deleteData(){
+  fetch(url + "/5",{
+    method: "DELETE",
+
+  })
+  .then((result)=> result.json()).then((data)=>{
+    console.log(data);
+  }).catch((error)=>{
+    console.log(error);
+  });
 }
 
-//reset functionality should  be executed
-function resetForm() {
-  document.getElementById("fullname").value = "";
-  document.getElementById("empCode").value = "";
-  document.getElementById("salary").value = "";
-  document.getElementById("city").value = "";
-  selectedRow = null;
-}
-
-//We will bring the data in form if user click on edit button
-function onEdit(td) {
-  selectedRow = td.parentElement.parentElement;
-  document.getElementById("fullname").value = selectedRow.cells[0].innerHTML;
-  document.getElementById("empCode").value = selectedRow.cells[1].innerHTML;
-  document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
-  document.getElementById("city").value = selectedRow.cells[3].innerHTML;
-}
-
-//Update the record after populating the data
-function updateRecord(formData) {
-  selectedRow.cells[0].innerHTML = formData.fullname;
-  selectedRow.cells[1].innerHTML = formData.empCode;
-  selectedRow.cells[2].innerHTML = formData.salary;
-  selectedRow.cells[3].innerHTML = formData.city;
-}
-
-function onDelete(td) {
-  if (confirm("Do you want to Delete this Employee Data ? ")) {
-    row = td.parentElement;
-    document.getElementById("employeeList").deleteRow(row.rowIndex);
-    resetForm();
-  }
-}
+deleteData();
