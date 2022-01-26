1fa72facbf6398460c35c706d1154711fd4c1e12
Abhay1kumar <kumarabhay401@gmail.com>
2022-01-26 23:41:36 +0530

    Cats Pic api

--- script.js
+++ script.js
@@ -1,86 +1,31 @@
-let button = document.querySelector('.btn');
+let mybtn = document.querySelector('.mybtn');
 
+mybtn.addEventListener('click', getCatPics);
 
+async function getCatPics() {
 
-// change font size 
+    // generate pic only one 
+    let showpic = document.querySelector('.ImgDiv')
+    showpic.innerHTML = '';
 
-button.addEventListener('click', function(){
-  let data = document.getElementsByTagName('P');
-  console.log(data);
-
-  for (i=0; i <data.length; i++) {
-    data[i].style.fontSize+="32px";
-  }
-  
-})
-
-// change font color
-
-
-button.addEventListener('click', function(){
-  let data = document.getElementsByTagName('P');
-  console.log(data);
- 
-  for (i=0; i <data.length; i++) {
-    data[i].style.color+="blue";
-  }
-  
-})
-
-// change the content 
-
-button.addEventListener('click', function(){
-  let data = document.getElementsByTagName('P');
-  console.log(data);
- 
-  for (i=0; i <data.length; i++) {
-    data[0].innerHTML="Content change by button";
-  }
-  
-})
-
-button.addEventListener('click', function(){
-  let data = document.getElementsByTagName('P');
-  console.log(data);
-  // data.getElementsByTagName('P')[1].innerHTML = 'My name is Rohit';
-  for (i=0; i <data.length; i++) {
-    data[1].style.opacity ="0";
-  }
-  
-})
-
-// first method showing error
-
-
-// let newbutton=document.queryCommandValue('.button');
-
-// newbutton.addEventListener('click',function(){
-//   document.querySelector('.new').innerHTML="Abhay";
-// })
-
-
-
-// second method
-  
-function change(){
-  document.querySelector('.new').innerHTML="abhay";
+    try {
+         await fetch("https://api.thecatapi.com/v1/images/search", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+            .then((response) => response.json()).then((data) => {
+                let imgUrl = data[0].url;//take only only url from the api object
+                // console.log(imgUrl);
+                let createImgElement = document.createElement('img');//Creat a img element
+                createImgElement.setAttribute('src', `${imgUrl}`);
+                let showpic = document.querySelector('.ImgDiv')
+                showpic.appendChild(createImgElement);
+            })
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
-
-
-
-
-let tablebtn = document.querySelector(".tablebtn");
-
-tablebtn.addEventListener('click',function(){
-let name=document.querySelector('#first_name').value;
-let city=document.querySelector('#city_name').value;
-
-console.log(name);
-console.log(city);
-
-  let tablerow=document.querySelector('.table').insertRow();
-  let firstrow=tablerow.insertCell(0);
-  let secondrow=tablerow.insertCell(1);
-  firstrow.innerHTML=`${name}`;
-  secondrow.innerHTML=`${city}`;
-})
+getCatPics();
