4cfd5abb175d45b28c722a1cf8f2c4b83489e7ce
Abhay1kumar <kumarabhay401@gmail.com>
2022-01-27 13:09:42 +0530

    TO do list

--- script.js
+++ script.js
@@ -1,34 +1,8 @@
+document.querySelector(".btn").addEventListener("click",function(){
+    let text=document.querySelector('.form-control').value;
+    console.log(text);
+    let newlist=document.createElement("li");
 
-
-
-async function getBooks() {
-
-    
-    var url = "https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyAsDqHTrD_5tfMfSg8r7rZ-V-pZUDZQa7M";
-    try{
-        await fetch(url,{
-            method:"GET",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-        .then((response) => response.json()).then((data) => {
-            console.log(data);
-            var items=data.items;
-            for(var i=0; i<items.length; i++){
-                console.log(items[i].volumeInfo);
-                display.innerHTML +=  `<div class = "result"><img src="${items[i].volumeInfo.imageLinks.smallThumbnail}">
-                
-         <p><u> ${items[i].volumeInfo.title} </u></p><br><p> ${items[i].volumeInfo.authors}</p> 
-        
-    </div><br><br><br>`
-
-            }
-        })
-
-        }catch(e) {
-            console.log(e);
-            display.innerHTML += "";
-        }
-}
-       
+    newlist.innerHTML=`<p><input type="checkbox" class="checkbox">${text}</p>`;
+    document.querySelector(".list").appendChild(newlist);
+});
